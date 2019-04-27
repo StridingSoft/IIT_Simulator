eee53ba233e82db32ef5cb1fd5f2e3f762f58606
Stasenash <staslisenash@gmail.com>
2019-04-27 15:29:27 +0500

    chose default animation, don't wanna fuck

--- IIT_Simulator/IIT_Simulator/IIT_Simulator/Menu.xaml.cs
+++ IIT_Simulator/IIT_Simulator/IIT_Simulator/Menu.xaml.cs
@@ -13,17 +13,19 @@ namespace IIT_Simulator
             NavigationPage.SetHasNavigationBar(this, false);
 		}
 
-        private void GameStart_Clicked(object sender, EventArgs e)
+        private async void GameStart_Clicked(object sender, EventArgs e)
         {
-            new Animation(opacity => this.Opacity = opacity / 100, 100, 0)
-            .Commit(this, "PageExitAnimation", 1, 350, Easing.CubicInOut, async(d, b) =>
-            {
-                var mainPage = new MainPage() { Opacity = 0 }; // Create the page with 0 opacity to animate later
-                await Navigation.PushAsync(mainPage, false); // Push the new page, as the current page is already with 0 opacity, without animation
-                await mainPage.FadeTo(1, 350, Easing.CubicInOut); // Animate the fade of the next page
-                this.Opacity = 100;
-            });
+            await Navigation.PushAsync(new MainPage());
+            //new Animation(opacity => this.Opacity = opacity / 100, 100, 0)
+            //.Commit(this, "PageExitAnimation", 1, 350, Easing.CubicInOut, async(d, b) =>
+            //{
+            //    var mainPage = new MainPage() { Opacity = 0 }; // Create the page with 0 opacity to animate later
+            //    await Navigation.PushAsync(mainPage, false); // Push the new page, as the current page is already with 0 opacity, without animation
+            //    await mainPage.FadeTo(1, 350, Easing.CubicInOut); // Animate the fade of the next page
+            //    this.Opacity = 100;
+            //});
         }
+
 
         private void Exit_Clicked(object sender, EventArgs e)
         {
